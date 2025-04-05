d9f581798b7c45754b4b5de35292f47fc84887b7
Eduardo <aristimdudu@gmail.com>
2025-04-05 03:35:58 -0300

    fix: sequelize capitalizando colunas, LINUX (railway) é case-sensitive)

--- models/impulso.js
+++ models/impulso.js
@@ -23,6 +23,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Impulso',
+    tableName: 'impulsos'
   });
   return Impulso;
 };
--- models/programa.js
+++ models/programa.js
@@ -21,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Programa',
+    tableName: 'programas'
   });
   return Programa;
 };
