ff74ce3417db478a42de0efe53c709c37df7b5ec
Eduardo <aristimdudu@gmail.com>
2025-04-05 02:09:32 -0300

    add: DB_PORT para config no railway

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -6,13 +6,15 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialect: "mysql",
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_PROD,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialect: "mysql",
   },
 };
